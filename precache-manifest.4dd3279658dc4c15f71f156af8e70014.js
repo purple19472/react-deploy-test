62ff4bc6ce15975c0bf74012432fec8ca59a9709
Elaina Smith <smit8561@umn.edu>
2020-05-11 19:19:02 -0500

    Updates

---
+++ precache-manifest.4dd3279658dc4c15f71f156af8e70014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db47fbd6978fbdfd084a0650faf51006",
+    "url": "/react-deploy-test/index.html"
+  },
+  {
+    "revision": "b14c5063d61487f77001",
+    "url": "/react-deploy-test/static/css/main.b68423c3.chunk.css"
+  },
+  {
+    "revision": "6d980d7a64185d03a069",
+    "url": "/react-deploy-test/static/js/2.c91fe6a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy-test/static/js/2.c91fe6a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b14c5063d61487f77001",
+    "url": "/react-deploy-test/static/js/main.a8227ff5.chunk.js"
+  },
+  {
+    "revision": "5f9664527f4e70e66626",
+    "url": "/react-deploy-test/static/js/runtime-main.ffb8b79b.js"
+  }
+]);
